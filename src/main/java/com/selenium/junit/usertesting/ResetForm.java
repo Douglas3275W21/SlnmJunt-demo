dc19c51bc68c41eb33da96e47aa377046dacaa42
Rez G <mrghaeli@yahoo.com>
2021-04-04 23:57:34 -0700

    first commit

---
+++ src/main/java/com/selenium/junit/usertesting/ResetForm.java
@@ -0,0 +1,14 @@
+package com.selenium.junit.usertesting;
+
+public class ResetForm {
+
+    private Integer value;
+
+    public Integer getValue() {
+        return value;
+    }
+
+    public void setValue(Integer value) {
+        this.value = value;
+    }
+}
